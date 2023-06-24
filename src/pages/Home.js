f78f982fd361e0650f37633c3e267b51a3754e6f
Fahad Tufail <tufailfahad01@gmail.com>
2023-06-24 16:48:59 +0500

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.homeContainer}>
+      <div>Home page</div>
+    </div>
+  );
+};
+
+export default Home;
